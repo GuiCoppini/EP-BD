30947819de4fc256a707d093fbbaadeb450a5dd7
GuiCoppini <guilhermecoppinip@gmail.com>
2017-11-09 22:45:56 -0200

    arruma erro de import

--- src/main/kotlin/SistemadeOcorrencias/Ocorrencia/OcorrenciaController.kt
+++ src/main/kotlin/SistemadeOcorrencias/Ocorrencia/OcorrenciaController.kt
@@ -1,6 +1,5 @@
 package SistemadeOcorrencias.Ocorrencia
 
-import SistemadeOcorrencias.Funcionario.Ocorrencia
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.RequestMapping
